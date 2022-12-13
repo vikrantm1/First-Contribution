d351968253953bfe5df97f39f7da6b11283bebd0
Vikrant Mandle <vikrantmandle12@gmail.com>
2022-12-13 17:51:52 +0530

    Added diff.java file

---
+++ diff.java
@@ -0,0 +1,31 @@
+public class diff {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+    
+        double a = in.nextDouble();
+    
+        double b = in.nextDouble();
+    
+        double value = a - b;
+    
+        System.out.println("Enter a: ");
+    
+        a = in.nextDouble();
+    
+        System.out.println("Enter b: ");
+    
+        b = in.nextDouble();
+    
+        //If value is negative...make it a positive number.
+    
+        value = (value < 0) ? -value : value;
+    
+        System.out.println(a + "-" + b + "=" + (a - b));
+    
+        System.out.println(b + "-" + a + "=" + (b - a));
+    
+    }
+    
+    }
+}
